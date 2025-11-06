ff06ef1ea507bf83fee7c781519cd7ed6ccd3622
gerald023 <lekaragerald19@gmail.com>
2025-11-05 21:33:33 -0800

    corrected the process section

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,7 +4,7 @@ import Loading from "../components/common/loading/Loading";
 const Home = lazy(() => import("../pages/Home"));
 const Main = lazy(() => import("../layouts/Main"));
 
-const repoName = import.meta.env.VITE_REPO_NAME || "";
+// const repoName = import.meta.env.VITE_REPO_NAME || "";
 
 export const router = createBrowserRouter(
   [
@@ -23,5 +23,5 @@ export const router = createBrowserRouter(
       ],
     },
   ],
-  { basename: `${repoName}` }
+  // { basename: `${repoName}` }
 );
